1820809b2a2890290d92bba0e27cdea061e85148
CHITTAJALLU PAVAN KUMAR <pavankumarchittajallu@zohomail.in>
2025-11-30 21:03:50 +0530

    Update bash_one_liners.sh

--- bash_one_liners.sh
+++ bash_one_liners.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 70 LPA BASH ONE-LINERS – DAY 1 (From public repos: onceupon/Bash-Oneliner + stephenturner/oneliners)
+# BASH ONE-LINERS – DAY 1 (From public repos: onceupon/Bash-Oneliner + stephenturner/oneliners)
 
 # 1. Extract any archive (tar, zip, etc.) in one command 
 extract () { if [ -f $1 ] ; then case $1 in *.tar.bz2)   tar xvjf $1    ;; *.tar.gz)    tar xvzf $1    ;; *.tar.xz)    tar Jxvf $1    ;; *.bz2)       bunzip2 $1     ;; *.rar)       unrar x $1     ;; *.gz)        gunzip $1      ;; *.tar)       tar xvf $1     ;; *.tbz2)      tar xvjf $1    ;; *.tgz)       tar xvzf $1    ;; *.zip)       unzip $1       ;; *.Z)         uncompress $1  ;; *.7z)        7z x $1        ;; *) echo "'$1' cannot be extracted via extract()" ;; esac else echo "'$1' is not a valid file"; fi }
